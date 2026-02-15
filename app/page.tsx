c02c642eff90394f1c1ec1720bd23d4d7e247704
Anubhav Biswas <anubhavbiswas25@gmail.com>
2026-02-15 20:39:05 +0530

    implemented coin overview and trending coins section ui and integrated from the api as well

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import CoinOverview from '@/Components/Home/CoinOverview'
+import TrendingCoins from '@/Components/Home/TrendingCoins'
+import { CoinOverviewFallback, TrendingCoinsFallback } from '@/Components/Home/fallback'
+import React, { Suspense } from 'react'
 
-function page() {
+// Dummy trending coins data
+// const trendingCoinsData: TrendingCoin[] = [
+//   {
+//     item: {
+//       id: 'bitcoin',
+//       name: 'Bitcoin',
+//       symbol: 'BTC',
+//       market_cap_rank: 1,
+//       thumb: '/assets/logo.svg',
+//       large: '/assets/logo.svg',
+//       data: {
+//         price: 42321.00,
+//         price_change_percentage_24h: {
+//           usd: 2.5
+//         }
+//       }
+//     }
+//   },
+//   {
+//     item: {
+//       id: 'ethereum',
+//       name: 'Ethereum',
+//       symbol: 'ETH',
+//       market_cap_rank: 2,
+//       thumb: '/assets/logo.svg',
+//       large: '/assets/logo.svg',
+//       data: {
+//         price: 2456.80,
+//         price_change_percentage_24h: {
+//           usd: -1.2
+//         }
+//       }
+//     }
+//   },
+//   {
+//     item: {
+//       id: 'cardano',
+//       name: 'Cardano',
+//       symbol: 'ADA',
+//       market_cap_rank: 3,
+//       thumb: '/assets/logo.svg',
+//       large: '/assets/logo.svg',
+//       data: {
+//         price: 0.98,
+//         price_change_percentage_24h: {
+//           usd: 3.8
+//         }
+//       }
+//     }
+//   },
+//   {
+//     item: {
+//       id: 'solana',
+//       name: 'Solana',
+//       symbol: 'SOL',
+//       market_cap_rank: 4,
+//       thumb: '/assets/logo.svg',
+//       large: '/assets/logo.svg',
+//       data: {
+//         price: 198.45,
+//         price_change_percentage_24h: {
+//           usd: -0.5
+//         }
+//       }
+//     }
+//   }
+// ]
+
+
+
+async function page() {
+  
+
+  
+  // the trending coins data is loaded only after coins details is loaded 
+  // now it we have multiple await calls we can use Promise.all to load them in parallel and
+  // reduce the loading time of the page for faster loading of the html below
+  // along with that we can also use suspense and loading UI to show the loading state of the page while the data is being fetched
+
   return (
-    <></>
+    <main className='main-container'>
+      <section className="home-grid">
+        {/* coin overview section */}
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview/>
+        </Suspense>
+        {/* trending coins */}
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins/>
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
---
+++ lib/coingecko.actions.ts
@@ -0,0 +1,39 @@
+'use server'
+
+import qs from 'query-string'
+
+const BASE_URL = process.env.GECKO_BASE_URL
+const API_KEY = process.env.GECKO_API_KEY
+
+if(!BASE_URL) throw new Error('Could not find GECKO_BASE_URL in the environment variables')
+if(!API_KEY) throw new Error('Could not find GECKO_API_KEY in the environment variables')
+
+export async function fetcher<T>(
+    endpoint:string,
+    params?: QueryParams,
+    revalidate = 60
+) : Promise<T>{
+    // fecthing data from coingecko api using the endpoint and params provided
+    const url = qs.stringifyUrl({
+        url: `${BASE_URL}/${endpoint}`,
+        query: params
+    }, {skipEmptyString: true, skipNull:true });
+
+    // fetching the data 
+    const response = await fetch(url,{
+        headers:{
+            "x_cg_demo_api_key":API_KEY,
+            "Content-Type":"application/json"
+        } as Record<string,string>,
+        next: {revalidate}// setting the cache revalidation time
+    })
+
+    if(!response.ok){
+        const errorBody: CoinGeckoErrorBody = await response.json()
+        .catch(()=>({}))
+
+        throw new Error(`API Error: ${response.status} : ${errorBody.error || response.statusText}`)
+    }
+
+    return response.json()
+}
